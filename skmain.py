1c6aef14bdfe18aee1e90bf97ff979831860b5c5
Trodis <galerian@gmx.net>
2016-03-21 08:26:31 +0100

    Only saving to excel is left!

--- skmain.py
+++ skmain.py
@@ -54,14 +54,55 @@ def request_concertpage(concert_link):
 
 def parse_concertpage(concert_page, artist_name):
     bs_obj = BeautifulSoup(concert_page.text, "lxml")
-    tickets_table = bs_obj.find('div', id='tickets')
     tickets_list = []
+    tickets_table = bs_obj.find('div', id='tickets')
     for tickets in tickets_table.find_all('div', class_='ticket-wrapper'):
-        for cell in tickets.find_all('div'):
-            print artist_name
-            print cell
-        
-        print "******************"
+        ticket_wrapper = tickets.find_all('div')
+        if ticket_wrapper[1].span is not None:
+            vendor = ticket_wrapper[0].span.text
+            ticket_price = ticket_wrapper[1].span.text
+            ticket_link = tickets.a['href']
+            tickets_list.append({'vendor':vendor, 'price': ticket_price, 'link': ticket_link})
+        else:
+            continue
+
+    address = []
+    venue_hcard = bs_obj.find('p', class_='venue-hcard').find_all('span')
+    for a in venue_hcard:
+        address.append(a.text)
+
+    additional_details = bs_obj.find('div', class_='additional-details-container')
+    if additional_details is not None:
+        additional_details = additional_details.p.text
+    else:
+        additional_details = False
+
+    return tickets_list, address, additional_details
+
+def write_sheet(artist_name, tickets, address, details, wb, ws):
+    row_number = ws.max_row+1
+    ws.cell(row=row_number, column=1, value=event_name)
+    ws.cell(row=row_number, column=2, value=event_facebook_url)
+    ws.cell(row=row_number, column=3, value=date_start)
+    ws.cell(row=row_number, column=4, value=ticket_uri)
+    ws.cell(row=row_number, column=5, value=city)
+    ws.cell(row=row_number, column=6, value=interested_count)
+    ws.cell(row=row_number, column=7, value=attending_count)
+    wb.save(EXCELFILE) 
+
+def create_sheet():
+    wb = Workbook()
+    ws = wb.active
+    ws.cell(row=1, column=1, value='Name').font = Font(bold=True)
+    ws.cell(row=1, column=2, value='Songkick URL').font = Font(bold=True)
+    ws.cell(row=1, column=3, value='Date Start').font = Font(bold=True)
+    ws.cell(row=1, column=4, value='Ticket Link').font = Font(bold=True)
+    ws.cell(row=1, column=5, value='City').font = Font(bold=True)
+    ws.cell(row=1, column=6, value='Address').font = Font(bold=True)
+    ws.cell(row=1, column=7, value='Additional Details').font = Font(bold=True)
+    wb.save(EXCELFILE)
+
+    return wb, ws 
 
 def parse_events(event_page):
     bs_obj = BeautifulSoup(event_page, "lxml")
@@ -71,7 +112,8 @@ def parse_events(event_page):
             artist_name = event.span.strong
             link = event.a['href']
             concert_page = request_concertpage(link)
-            parse_concertpage(concert_page, artist_name)
+            tickets, address, details = parse_concertpage(concert_page, artist_name)
+            save_scraped(artist_name, tickets, address, details)
         else:
             continue
 
